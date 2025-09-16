063293fbbac66f4c4dd73ddd2ab07e2372be2adc
Sharjeel14 <sharjeelr117@gmail.com>
2025-09-17 00:35:10 +0500

    add s

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 8070
 
 app.get('/', (req, res) => {
-    res.send('Hello World! BSITM it')
+    res.send('Hello World! BSITM it s')
 })
 
 app.listen(port, () => {
