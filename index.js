161226b811657e6751d3acfb335c9bf305f5d63e
Sharjeel14 <sharjeelr117@gmail.com>
2025-09-17 00:31:16 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 8070
+
+app.get('/', (req, res) => {
+    res.send('Hello World! BSITM it')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
